d8bbb2284152d66b04de38c9ab00e590b423149e
AbimaelG <azg_96@hotmail.com>
2022-02-13 19:50:40 -0600

    branch todoListsUsingObjects created

--- docs/scripts/app.js
+++ docs/scripts/app.js
@@ -10,29 +10,38 @@ function createList() {
 
 // Close window and creates new todo-list
 function createNameList() {
-    const newList = document.createElement('div')
-    const newH1 = document.createElement('h1')
-    const newDiv = document.createElement('div')
-    const newCheckbox = document.createElement('input')
-    const newTodoText = document.createElement('input')
-    const selectText = document.getElementById('list-name').value
-    const newListTextName = document.createTextNode(selectText)
-
-    newDiv.setAttribute('class', 'list-name')
-    newCheckbox.setAttribute('type', 'checkbox')
-    newTodoText.setAttribute('type', 'text')
-
-    if (!selectText) {
-        alert('Necesitas poner un texto')
-    } else {
-        // Creates text and adds it to the element 
-        newH1.appendChild(newListTextName)
-        newList.appendChild(newH1)
-        newList.appendChild(newDiv)
-        newDiv.appendChild(newCheckbox)
-        newDiv.appendChild(newTodoText)
-        document.body.appendChild(newList)
-
-        document.getElementById('creation-table').removeAttribute('style')
+    const nameList = document.getElementById('list-name').value
+    const ovbio = {
+        nameList: nameList,
     }
+    
+    console.log(ovbio)
 }
+
+// function createNameList() {
+//     const newList = document.createElement('div')
+//     const newH1 = document.createElement('h1')
+//     const newDiv = document.createElement('div')
+//     const newCheckbox = document.createElement('input')
+//     const newTodoText = document.createElement('input')
+//     const selectText = document.getElementById('list-name').value
+//     const newListTextName = document.createTextNode(selectText)
+
+//     newDiv.setAttribute('class', 'list-name')
+//     newCheckbox.setAttribute('type', 'checkbox')
+//     newTodoText.setAttribute('type', 'text')
+
+//     if (!selectText) {
+//         alert('Necesitas poner un texto')
+//     } else {
+//         // Creates text and adds it to the element 
+//         newH1.appendChild(newListTextName)
+//         newList.appendChild(newH1)
+//         newList.appendChild(newDiv)
+//         newDiv.appendChild(newCheckbox)
+//         newDiv.appendChild(newTodoText)
+//         document.body.appendChild(newList)
+
+//         document.getElementById('creation-table').removeAttribute('style')
+//     }
+// }
